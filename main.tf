c0171632896ff33249fa25d6da4ba77ddb441004
gouritasgaonkar <131243004+gouritasgaonkar@users.noreply.github.com>
2023-10-03 12:51:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,74 +1,77 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
+required_providers {
+aws = {
+source = "hashicorp/aws"
+version = "~> 4.0"
 }
+}
+}
+variable "accesskey" {}
+variable "secretkey" {}
 provider "aws" {
-  access_key="var.accesskey"
-  secret_key= "var.secretkey"
-  region     = "ap-south-1"
+region= "ap-south-1"
+access_key = var.accesskey
+secret_key = var.secretkey
+}
+variable "region_name" {
+type=string
+default= "ap-south-1"
+}
+variable "server_port" {
+type= number
+default=80
+}
+variable "publiccidr" {
+type = list(string)
+default = ["0.0.0.0/0"]
 }
 resource "aws_instance" "myec2" {
-  ami           = "ami-03a6eaae9938c858c"
-  instance_type = "t2.micro"
-  vpc_security_group_ids=[aws_security_group.web-sg.id]
-  key_name="tf-key-pair"
-
- 
-
- 
-
-tags={
- Name="web-server"
+ami = "ami-0d81306eddc614a45"
+instance_type = "t2.micro"
+vpc_security_group_ids = [aws_security_group.ownsg.id]
+key_name = "tf-key-pair"
+tags = {
+Name = "terraform-example"
 }
-user_data= <<-EOF
+user_data = <<-EOF
 #!/bin/bash
 yum install httpd -y
 service httpd start
 cd /var/www/html
 touch index.html
-echo "hello from Terraform" > index.html
+echo "hello from terraform" > index.html
 EOF
 }
-resource "aws_security_group" "web-sg" {
- name="web-sg"
+resource "aws_security_group" "ownsg" {
+name = "own-sg"
 ingress {
- from_port=80
- to_port=80
-protocol="tcp"
-cidr_blocks= ["0.0.0.0/0"]
+from_port = var.server_port
+to_port = var.server_port
+protocol = "tcp"
+cidr_blocks = var.publiccidr
 }
-
- 
-
- 
-
 ingress {
- from_port=22
- to_port=22
-protocol="tcp"
-cidr_blocks= ["0.0.0.0/0"]
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
 }
 egress {
- from_port=0
- to_port=0
-protocol="-1"
-cidr_blocks= ["0.0.0.0/0"]
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
 }
 }
-resource "aws_key_pair" "tf-key-pair-1" {
+resource "aws_key_pair" "tf-key-pair" {
 key_name = "tf-key-pair"
 public_key = tls_private_key.rsa.public_key_openssh
 }
 resource "tls_private_key" "rsa" {
 algorithm = "RSA"
-rsa_bits  = 4096
+rsa_bits = 4096
 }
 resource "local_file" "tf-key" {
-content  = tls_private_key.rsa.private_key_pem
-filename = "tf-key-pair-1"
+content = tls_private_key.rsa.private_key_pem
+filename = "tf-key-pair"
 }
- 
